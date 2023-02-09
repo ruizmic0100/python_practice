e270fdefe02a834518be1eebb133fa6dd3d2dfd8
ruizmic0100 <ruizmic0100@hotmail.com>
2023-02-09 12:32:47 -0600

    Init commit with basic python datatypes and manipulations of strings and numbers.

---
+++ main/main.py
@@ -0,0 +1,26 @@
+# User input:
+
+# first_name = "Michael"
+# last_name = "Ruiz"
+# full_name = first_name + " " + last_name
+# print("Hello" + full_name)
+# print(type(first_name))
+# print(len(last_name))
+# print(first_name.find("M"))
+# print(first_name.capitalize())
+# print(first_name.upper())
+# print(first_name.lower())
+# print(first_name.isdigit())
+# print(first_name.isalpha())
+# print(first_name.count("i"))
+# print(first_name.replace("c", "p"))
+# print(first_name * 4)
+
+## User input:
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+
+age = age + 1
+
+print("Hello " + name)
+print("You are " + str(age) + " years old")
