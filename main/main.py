e21211f283525fd155b090624d27acf0991e6179
Michael <ruizmic0100@hotmail.com>
2023-02-09 22:43:05 -0600

    Progress today

--- main/main.py
+++ main/main.py
@@ -17,10 +17,77 @@
 # print(first_name * 4)
 
 ## User input:
-name = input("What is your name?: ")
-age = int(input("How old are you?: "))
+# name = input("What is your name?: ")
+# age = int(input("How old are you?: "))
 
-age = age + 1
+# age = age + 1
 
-print("Hello " + name)
-print("You are " + str(age) + " years old")
+# print("Hello " + name)
+# print("You are " + str(age) + " years old")
+
+# import math
+
+# pi = 3.14
+# x = 1
+# y = 2
+# z = 3
+
+# print(round(pi))
+# print(math.ceil(pi))
+# print(math.floor(pi))
+# print(abs(pi))
+# print(pow(pi,2))
+# print(math.sqrt(420))
+# print(max(x,y,z))
+# print(min(x,y,z))
+
+# slicing : create a substring by extracting elements from another string
+#           indexing[] or slice()
+#           [start:stop:step]
+
+# name = "Michael 1024"
+
+# first_name = name[0:6]
+# last_name = name[7:11]
+# reversed_name = name[::-1]
+
+# print(first_name)
+# print(last_name)
+# print(reversed_name)
+
+# website = "http://google.com"
+# website2 = "http://wikipedia.com"
+
+# slice = slice(7,-4)
+
+# print(website[slice])
+# print(website2[slice])
+
+# if statement = a block of code that will execute if it's condition is true.
+
+# age = int(input("How old are you?: "))
+
+# if age >= 18:
+#     print("You are an adult!")
+# elif age == 100:
+#     print("You are a century old!")
+# elif age < 0:
+#     print("You haven't been born yet!")
+# else:
+#     print("You are a child!")
+
+# logical operators (and,or,not) = used to check if two or more conditional statements are true.
+
+# temp = int(input("what is the temperature outside?: "))
+
+# if temp >= 0 and temp <= 30:
+#     print("The temperature is good today!")
+#     print("go outside!")
+# elif temp < 0 or temp > 30:
+#     print("The temperature is bad today!")
+#     print("stay outside!")
+
+# while loop = a stastement that will execute it's block of code as long as it's condition remains true.
+
+while 1 == 1:
+    print("Help I am an infinite loop!")
