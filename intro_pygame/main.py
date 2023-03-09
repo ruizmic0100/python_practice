04cd85a5560e7189eabcf196f76a26bf356d424c
ruizmic0100 <ruizmic0100@hotmail.com>
2023-03-08 18:25:09 -0600

    Working through the ultimate introduction to pygame video so that I can practice python.

--- intro_pygame/main.py
+++ intro_pygame/main.py
@@ -4,6 +4,18 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
+sky_surf = pygame.image.load('graphics/Sky.png').convert()
+ground_surf = pygame.image.load('graphics/ground.png').convert()
+text_surf = test_font.render('My game', False, 'Black')
+
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(bottomright = (600, 300))
+
+player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300))
 
 while True:
     for event in pygame.event.get():
@@ -11,6 +23,15 @@ while True:
             pygame.quit()
             exit()
 
-    # draw all our elements
-    # update everything
+    screen.blit(sky_surf, (0,0))
+    screen.blit(ground_surf, (0, 300))
+    screen.blit(text_surf, (300, 50))
+    snail_rect.x -= 4
+    if snail_rect.right <= 0: snail_rect.left = 800
+    screen.blit(snail_surf, snail_rect)
+    print(player_rect.left)
+    screen.blit(player_surf, player_rect)
+
     pygame.display.update()
+    clock.tick(60)
+
